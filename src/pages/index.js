94a08b8fa670e0a22e98d26f8170df1e62213723
Dulce Hernandez <dulce.hernandezc@gmail.com>
2019-04-20 20:47:00 -0500

    Query data from Sample page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,41 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { graphql, StaticQuery } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+
+  const handleRenderPage = props => (
+    <div>
+      {props.allWordpressPage.edges.map(page => (
+        <div key={page.node.id}>
+          <h1>{page.node.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: page.node.content}} />
+        </div>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+  )
+  return (
+  <Layout>
+    <StaticQuery
+      query={
+        graphql`
+        {
+          allWordpressPage {
+            edges {
+              node {
+                id
+                title
+                content
+              }
+            }
+          }
+        }
+      `}
+      render={handleRenderPage}
+    />
   </Layout>
-)
+  )
+}
 
 export default IndexPage
