f3c784fdd2795a19f1933fbb03bde98cffde976e
Dulce Hernandez <dulce.hernandezc@gmail.com>
2019-06-02 19:47:34 -0500

    Render portfolio under content theme

--- gatsby-node.js
+++ gatsby-node.js
@@ -75,6 +75,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   // Create Page pages.
   const pageTemplate = path.resolve(`./src/templates/page.js`)
+  const portfolioUnderContentTemplate = path.resolve(`./src/templates/portfolioUnderContent.js`)
   // We want to create a detailed page for each page node.
   // The path field contains the relative original WordPress link
   // and we use it for the slug to preserve url structure.
@@ -89,7 +90,7 @@ exports.createPages = async ({ graphql, actions }) => {
       // optional but is often necessary so the template
       // can query data specific to each page.
       path: edge.node.path,
-      component: slash(pageTemplate),
+      component: slash(edge.node.template === 'portfolio_under_content.php' ? portfolioUnderContentTemplate : pageTemplate),
       context: edge.node,
     })
   })
--- src/templates/portfolioUnderContent.js
+++ src/templates/portfolioUnderContent.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
+import PortfolioItems from '../components/PortfolioItems'
 
-const Page = ({ pageContext }) => {
+export default ({ pageContext }) => {
   const { title, content } = pageContext
   return (
     <Layout>
       <h1>{title}</h1>
       <div dangerouslySetInnerHTML={{__html: content}} />
+      <PortfolioItems />
     </Layout>
   )
 }
-
-export default Page
