d44cf85bd8fe2657656972bcf9e185578fc5a7ce
Dulce Hernandez <dulce.hernandezc@gmail.com>
2019-06-09 18:10:29 -0500

    Change wordpress domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,9 +35,9 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "myportfolio.lndo.site",
+        baseUrl: "test.dulcehernandezc.com",
         // The protocol. This can be http or https.
-        protocol: "http",
+        protocol: "https",
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
